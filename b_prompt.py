8aaf96bfa6bceabf051a0719bc43986aa005e188
peppingdore <butichex@gmail.com>
2020-08-09 19:14:48 +0300

    Faster git branch detection

--- b_prompt.py
+++ b_prompt.py
@@ -5,12 +5,27 @@ import subprocess
 from pathlib import Path
 import ascii_colors
 
+def get_active_branch_name():
+
+    head_dir = Path(".") / ".git" / "HEAD"
+    with head_dir.open("r") as f: content = f.read().splitlines()
+
+    for line in content:
+        if line[0:4] == "ref:":
+            return line.partition("refs/heads/")[2]
+
 def b_prompt():
 	current_path = Path.cwd().resolve()
 	sys.stdout.write(f'{ascii_colors.yellow}{current_path}{ascii_colors.reset_foreground_color}')
 
-	current_branch = subprocess.run('git branch --show-current', shell = True, stdout = subprocess.PIPE).stdout.decode('utf-8')
-	current_branch = current_branch.rstrip('\n')
+	current_branch = ''
+	try:
+		current_branch = get_active_branch_name()
+	except:
+		pass
+
+	#current_branch = subprocess.run('git branch --show-current', shell = True, stdout = subprocess.PIPE).stdout.decode('utf-8')
+	#current_branch = current_branch.rstrip('\n')
 
 	if len(current_branch):
 		sys.stdout.write(f' {ascii_colors.green.background}{current_branch}{ascii_colors.reset_background_color}')
